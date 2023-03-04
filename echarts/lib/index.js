331faa4730d7832af2b9e9b30478aa7a4f47aa0a
korealu <643949593@qq.com>
2023-03-04 12:45:54 +0800

    echarts-地图

---
+++ echarts/lib/index.js
@@ -0,0 +1,377 @@
+const listData = [{
+    name: 'apple',
+    count: 3000,
+    month: '202201'
+  },
+  {
+    name: 'apple',
+    count: 4000,
+    month: '202202'
+  },
+  {
+    name: 'apple',
+    count: 3800,
+    month: '202203'
+  },
+  {
+    name: 'apple',
+    count: 4200,
+    month: '202204'
+  },
+  {
+    name: 'apple',
+    count: 5000,
+    month: '202205'
+  },
+  {
+    name: 'apple',
+    count: 4100,
+    month: '202206'
+  },
+  {
+    name: 'apple',
+    count: 3900,
+    month: '202207'
+  },
+  {
+    name: 'apple',
+    count: 3200,
+    month: '202208'
+  },
+  {
+    name: 'apple',
+    count: 3300,
+    month: '202209'
+  },
+  {
+    name: 'apple',
+    count: 4300,
+    month: '202210'
+  },
+  {
+    name: 'apple',
+    count: 4400,
+    month: '202211'
+  },
+  {
+    name: 'apple',
+    count: 3750,
+    month: '202212'
+  },
+  {
+    name: 'pear',
+    count: 6000,
+    month: '202201'
+  },
+  {
+    name: 'pear',
+    count: 5000,
+    month: '202202'
+  },
+  {
+    name: 'pear',
+    count: 3800,
+    month: '202203'
+  },
+  {
+    name: 'pear',
+    count: 7200,
+    month: '202204'
+  },
+  {
+    name: 'pear',
+    count: 5500,
+    month: '202205'
+  },
+  {
+    name: 'pear',
+    count: 4800,
+    month: '202206'
+  },
+  {
+    name: 'pear',
+    count: 2300,
+    month: '202207'
+  },
+  {
+    name: 'pear',
+    count: 3200,
+    month: '202208'
+  },
+  {
+    name: 'pear',
+    count: 2300,
+    month: '202209'
+  },
+  {
+    name: 'pear',
+    count: 5300,
+    month: '202210'
+  },
+  {
+    name: 'pear',
+    count: 6400,
+    month: '202211'
+  },
+  {
+    name: 'pear',
+    count: 5750,
+    month: '202212'
+  },
+  {
+    name: 'banana',
+    count: 3000,
+    month: '202201'
+  },
+  {
+    name: 'banana',
+    count: 4000,
+    month: '202202'
+  },
+  {
+    name: 'banana',
+    count: 3800,
+    month: '202203'
+  },
+  {
+    name: 'banana',
+    count: 4200,
+    month: '202204'
+  },
+  {
+    name: 'banana',
+    count: 5000,
+    month: '202205'
+  },
+  {
+    name: 'banana',
+    count: 4100,
+    month: '202206'
+  },
+  {
+    name: 'banana',
+    count: 3900,
+    month: '202207'
+  },
+  {
+    name: 'banana',
+    count: 3200,
+    month: '202208'
+  },
+  {
+    name: 'banana',
+    count: 3300,
+    month: '202209'
+  },
+  {
+    name: 'banana',
+    count: 4300,
+    month: '202210'
+  },
+  {
+    name: 'banana',
+    count: 4400,
+    month: '202211'
+  },
+  {
+    name: 'banana',
+    count: 3750,
+    month: '202212'
+  },
+  {
+    name: 'peach',
+    count: 7000,
+    month: '202201'
+  },
+  {
+    name: 'peach',
+    count: 5000,
+    month: '202202'
+  },
+  {
+    name: 'peach',
+    count: 4800,
+    month: '202203'
+  },
+  {
+    name: 'peach',
+    count: 4600,
+    month: '202204'
+  },
+  {
+    name: 'peach',
+    count: 7200,
+    month: '202205'
+  },
+  {
+    name: 'peach',
+    count: 3100,
+    month: '202206'
+  },
+  {
+    name: 'peach',
+    count: 4900,
+    month: '202207'
+  },
+  {
+    name: 'peach',
+    count: 2200,
+    month: '202208'
+  },
+  {
+    name: 'peach',
+    count: 3700,
+    month: '202209'
+  },
+  {
+    name: 'peach',
+    count: 8300,
+    month: '202210'
+  },
+  {
+    name: 'peach',
+    count: 5500,
+    month: '202211'
+  },
+  {
+    name: 'peach',
+    count: 2750,
+    month: '202212'
+  },
+  {
+    name: 'lemon',
+    count: 3120,
+    month: '202201'
+  },
+  {
+    name: 'lemon',
+    count: 4800,
+    month: '202202'
+  },
+  {
+    name: 'lemon',
+    count: 2345,
+    month: '202203'
+  },
+  {
+    name: 'lemon',
+    count: 3412,
+    month: '202204'
+  },
+  {
+    name: 'lemon',
+    count: 6432,
+    month: '202205'
+  },
+  {
+    name: 'lemon',
+    count: 3900,
+    month: '202206'
+  },
+  {
+    name: 'lemon',
+    count: 2900,
+    month: '202207'
+  },
+  {
+    name: 'lemon',
+    count: 4400,
+    month: '202208'
+  },
+  {
+    name: 'lemon',
+    count: 6500,
+    month: '202209'
+  },
+  {
+    name: 'lemon',
+    count: 2300,
+    month: '202210'
+  },
+  {
+    name: 'lemon',
+    count: 2400,
+    month: '202211'
+  },
+  {
+    name: 'lemon',
+    count: 2750,
+    month: '202212'
+  },
+  {
+    name: 'corn',
+    count: 4600,
+    month: '202201'
+  },
+  {
+    name: 'corn',
+    count: 6500,
+    month: '202202'
+  },
+  {
+    name: 'corn',
+    count: 3200,
+    month: '202203'
+  },
+  {
+    name: 'corn',
+    count: 7200,
+    month: '202204'
+  },
+  {
+    name: 'corn',
+    count: 5000,
+    month: '202205'
+  },
+  {
+    name: 'corn',
+    count: 8800,
+    month: '202206'
+  },
+  {
+    name: 'corn',
+    count: 3900,
+    month: '202207'
+  },
+  {
+    name: 'corn',
+    count: 1100,
+    month: '202208'
+  },
+  {
+    name: 'corn',
+    count: 1200,
+    month: '202209'
+  },
+  {
+    name: 'corn',
+    count: 1200,
+    month: '202210'
+  },
+  {
+    name: 'corn',
+    count: 1300,
+    month: '202211'
+  },
+  {
+    name: 'corn',
+    count: 1750,
+    month: '202212'
+  },
+
+]
+
+function mapDataKey(arr, key) {
+  const obj = {};
+  arr.forEach((item) => {
+    let value = item[key];
+    obj[value] ? obj[value].push(item) : (obj[value] = [item]);
+  });
+  return Object.keys(obj).map((item) => {
+    return {
+      [item]: obj[item]
+    }
+  });
+}
+
+console.log(mapDataKey(listData, 'name'));
